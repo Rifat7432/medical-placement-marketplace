f2a2f48670380bf05c31ee34f9cc84fa1c974d11
Rifat7432 <mdr341762@gmail.com>
2026-05-08 04:14:56 +0600

    add update hospital api

--- src/app/modules/hospital/hospital.controller.ts
+++ src/app/modules/hospital/hospital.controller.ts
@@ -4,70 +4,68 @@ import sendResponse from '../../../shared/sendResponse';
 import { HospitalService } from './hospital.service';
 import AppError from '../../../errors/AppError';
 
-
-
 const getHospitals = catchAsync(async (req, res) => {
-  const result = await HospitalService.getHospitals();
-  sendResponse(res, {
-    success: true,
-    statusCode: StatusCodes.OK,
-    message: 'Hospitals retrieved successfully',
-    data: result,
-  });
+     const result = await HospitalService.getHospitals();
+     sendResponse(res, {
+          success: true,
+          statusCode: StatusCodes.OK,
+          message: 'Hospitals retrieved successfully',
+          data: result,
+     });
 });
 
 const getHospitalProfile = catchAsync(async (req, res) => {
-  const user: any = req.user;
-  const result = await HospitalService.getHospitalProfile(user.id);
-  sendResponse(res, {
-    success: true,
-    statusCode: StatusCodes.OK,
-    message: 'Hospital profile retrieved successfully',
-    data: result,
-  });
+     const user: any = req.user;
+     const result = await HospitalService.getHospitalProfile(user.id);
+     sendResponse(res, {
+          success: true,
+          statusCode: StatusCodes.OK,
+          message: 'Hospital profile retrieved successfully',
+          data: result,
+     });
 });
 
 const getHospital = catchAsync(async (req, res) => {
-  const { id } = req.params;
-  const result = await HospitalService.getHospitalById(id);
-  if (!result) {
-    throw new AppError(StatusCodes.NOT_FOUND, 'Hospital not found');
-  }
-  sendResponse(res, {
-    success: true,
-    statusCode: StatusCodes.OK,
-    message: 'Hospital retrieved successfully',
-    data: result,
-  });
+     const { id } = req.params;
+     const result = await HospitalService.getHospitalById(id);
+     if (!result) {
+          throw new AppError(StatusCodes.NOT_FOUND, 'Hospital not found');
+     }
+     sendResponse(res, {
+          success: true,
+          statusCode: StatusCodes.OK,
+          message: 'Hospital retrieved successfully',
+          data: result,
+     });
 });
 
 const updateHospital = catchAsync(async (req, res) => {
-  const { id } = req.params;
-  const updateData = req.body;
-  const result = await HospitalService.updateHospital(id, updateData);
-  if (!result) {
-    throw new AppError(StatusCodes.NOT_FOUND, 'Hospital not found');
-  }
-  sendResponse(res, {
-    success: true,
-    statusCode: StatusCodes.OK,
-    message: 'Hospital updated successfully',
-    data: result,
-  });
+     const { id } = req.params;
+     const updateData = req.body;
+     const result = await HospitalService.updateHospital(id, updateData);
+     if (!result) {
+          throw new AppError(StatusCodes.NOT_FOUND, 'Hospital not found');
+     }
+     sendResponse(res, {
+          success: true,
+          statusCode: StatusCodes.OK,
+          message: 'Hospital updated successfully',
+          data: result,
+     });
 });
 
 const deleteHospital = catchAsync(async (req, res) => {
-  const { id } = req.params;
-  const result = await HospitalService.deleteHospital(id);
-  if (!result) {
-    throw new AppError(StatusCodes.NOT_FOUND, 'Hospital not found');
-  }
-  sendResponse(res, {
-    success: true,
-    statusCode: StatusCodes.OK,
-    message: 'Hospital deleted successfully',
-    data: result,
-  });
+     const { id } = req.params;
+     const result = await HospitalService.deleteHospital(id);
+     if (!result) {
+          throw new AppError(StatusCodes.NOT_FOUND, 'Hospital not found');
+     }
+     sendResponse(res, {
+          success: true,
+          statusCode: StatusCodes.OK,
+          message: 'Hospital deleted successfully',
+          data: result,
+     });
 });
 const hospitalOverview = catchAsync(async (req, res) => {
      const result = await HospitalService.hospitalOverview(req.user.id);
@@ -75,13 +73,11 @@ const hospitalOverview = catchAsync(async (req, res) => {
      sendResponse(res, { success: true, statusCode: StatusCodes.OK, message: 'Hospital overview retrieved successfully', data: result });
 });
 export const HospitalController = {
+     getHospitals,
+     getHospital,
+     updateHospital,
+     deleteHospital,
 
-  getHospitals,
-  getHospital,
-  updateHospital,
-  deleteHospital,
-
-
-  getHospitalProfile,
-  hospitalOverview
+     getHospitalProfile,
+     hospitalOverview,
 };
