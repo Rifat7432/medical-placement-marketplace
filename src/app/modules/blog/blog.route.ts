89a77156791c5e82d7fcdf97dc5df8b677109a69
Rifat7432 <mdr341762@gmail.com>
2026-08-26 22:32:00 +0600

    update

--- src/app/modules/blog/blog.route.ts
+++ src/app/modules/blog/blog.route.ts
@@ -1,13 +1,45 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import { BlogController } from './blog.controller';
 import { BlogValidation } from './blog.validation';
 import validateRequest from '../../middleware/validateRequest';
 import auth from '../../middleware/auth';
 import { USER_ROLES } from '../../../enums/user';
+import fileUploadHandler from '../../middleware/fileUploadHandler';
+import uploadMulterFilesToS3 from '../../middleware/uploadMulterFilesToS3';
+import { StatusCodes } from 'http-status-codes';
+import AppError from '../../../errors/AppError';
 
 const router = express.Router();
 
-router.post('/', auth(USER_ROLES.ADMIN), validateRequest(BlogValidation.createBlogZodSchema), BlogController.createBlog);
+router.post('/', fileUploadHandler(),
+     async (req: Request, res: Response, next: NextFunction) => {
+          try {
+               const uploadedFiles = await uploadMulterFilesToS3(req.files as Record<string, Express.Multer.File[]>);
+
+               let image = null;
+
+               for (const fieldName in uploadedFiles) {
+                    const value = uploadedFiles[fieldName];
+
+                    if (Array.isArray(value)) {
+                         throw new AppError(StatusCodes.BAD_REQUEST, `Multiple files uploaded Profile Image, expected only one.`);
+                    } else {
+                         image = value.url;
+                    }
+               }
+
+               const data = JSON.parse(req.body?.data || '{}');
+
+               req.body = {
+                    ...data,
+                   ...( image && { image })
+               };
+
+               next();
+          } catch (error) {
+               next(error);
+          }
+     }, auth(USER_ROLES.ADMIN), validateRequest(BlogValidation.createBlogZodSchema), BlogController.createBlog);
 router.get('/', BlogController.getBlogs);
 router.get('/:id', BlogController.getSingleBlog);
 
--- src/app/modules/blog/blog.validation.ts
+++ src/app/modules/blog/blog.validation.ts
@@ -3,6 +3,7 @@ import { z } from 'zod';
 const createBlogZodSchema = z.object({
   body: z.object({
     title: z.string({ required_error: 'Title is required' }),
+    image: z.string({ required_error: 'Image is required' }),
     content: z.string({ required_error: 'Content is required' }),
   }),
 });
